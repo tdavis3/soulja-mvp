26f6be4cbc61fa731db8b8190faf18989b918e4c
Evan Mays <me@evanmays.com>
2021-03-20 16:33:58 -0400

    use sodmg logo

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -2,12 +2,11 @@ import React from 'react';
 import './Header.css';
 import ConnectWallet from "./ConnectWallet";
 
-
 const Header = () => {
     return (
         <div className="Header">
             <a href="/">
-                Logo Here
+                <img src={process.env.PUBLIC_URL+"/sodmglogo.png"} />
             </a>
             <div style={{flexGrow: 1}}/>
             <ConnectWallet/>
