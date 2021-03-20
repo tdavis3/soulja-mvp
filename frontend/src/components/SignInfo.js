062bc34eb257a48cd03abc2d80937acf687baae7
Tyrone Davis III <td3@mit.edu>
2021-03-20 18:57:28 -0400

    added album sign info on landing

---
+++ frontend/src/components/SignInfo.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const SignInfo = () => {
+    return (
+        <div>
+            <h2>Get Your Album Signed</h2>
+            <div>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip
+                    ex ea commodo consequat.
+                </p>
+                {/*TODO: Signature pic here*/}
+            </div>
+            <h4>Select an NFT to sign</h4>
+            {/*TODO: ListView of copies*/}
+        </div>
+    );
+}
+
+export default SignInfo;
