e8755e70977f228b3ffe6773acda0066e82fbaad
Scott Moses Sunarto <github@smsunarto.com>
2021-03-21 01:01:30 +0700

    bug fix and stuff

--- hardhat/contracts/SouljaSignatureRegistrar.sol
+++ hardhat/contracts/SouljaSignatureRegistrar.sol
@@ -5,6 +5,26 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/IERC721.sol";
 
 contract SignatureRegistrar is Ownable {
+    event SignRequestCreated(
+        address nftContract,
+        uint256 nftID,
+        address signer,
+        address requester
+    );
+    event SignRequestCancelled(
+        address nftContract,
+        uint256 nftID,
+        address signer,
+        address requester
+    );
+    event Signed(
+        address nftContract,
+        uint256 nftID,
+        uint256 signatureID,
+        address signer,
+        address requester
+    );
+
     struct Signature {
         address signer;
         string signatureFile;
@@ -12,8 +32,9 @@ contract SignatureRegistrar is Ownable {
 
     Signature[] public signatures;
 
-    mapping(address => mapping(uint256 => mapping(address => bool))) canSign;
-    mapping(address => mapping(uint256 => uint256)) nftToSignatureID;
+    mapping(address => mapping(uint256 => mapping(address => bool)))
+        public canSign;
+    mapping(address => mapping(uint256 => uint256)) public nftToSignatureID;
 
     constructor() {
         // Fill index 0 to identify unsigned NFTs
@@ -32,7 +53,20 @@ contract SignatureRegistrar is Ownable {
 
         signatures.push(Signature(msg.sender, "temp"));
 
-        nftToSignatureID[nftContract][nftID] = signatures.length - 1;
+        uint256 signatureID = signatures.length - 1;
+
+        nftToSignatureID[nftContract][nftID] = signatureID;
+
+        canSign[nftContract][nftID][msg.sender] = false;
+
+        IERC721 nft = IERC721(nftContract);
+        emit Signed(
+            nftContract,
+            nftID,
+            signatureID,
+            msg.sender,
+            nft.ownerOf(nftID)
+        );
     }
 
     function createSignatureRequest(
@@ -42,15 +76,20 @@ contract SignatureRegistrar is Ownable {
     ) public {
         IERC721 nft = IERC721(nftContract);
         require(
-            nft.ownerOf(nftID) == msg.sender,
-            "SignatureRegistrar: msg,sender is not the owner of this NFT"
+            nftToSignatureID[nftContract][nftID] == 0,
+            "SignatureRegistrar: NFT already signed"
         );
         require(
-            canSign[nftContract][nftID][signer],
+            nft.ownerOf(nftID) == msg.sender,
+            "SignatureRegistrar: msg.sender is not the owner of this NFT"
+        );
+        require(
+            !canSign[nftContract][nftID][signer],
             "SignatureRegistrar: signer can sign already"
         );
 
         canSign[nftContract][nftID][signer] = true;
+        emit SignRequestCreated(nftContract, nftID, signer, msg.sender);
     }
 
     function cancelSignatureRequest(
@@ -61,7 +100,7 @@ contract SignatureRegistrar is Ownable {
         IERC721 nft = IERC721(nftContract);
         require(
             nft.ownerOf(nftID) == msg.sender,
-            "SignatureRegistrar: msg,sender is not the owner of this NFT"
+            "SignatureRegistrar: msg.sender is not the owner of this NFT"
         );
         require(
             canSign[nftContract][nftID][signer],
@@ -69,5 +108,6 @@ contract SignatureRegistrar is Ownable {
         );
 
         canSign[nftContract][nftID][signer] = false;
+        emit SignRequestCancelled(nftContract, nftID, signer, msg.sender);
     }
 }
