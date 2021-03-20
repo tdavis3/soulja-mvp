e8b2bfbbf048aea201aa6d12b52429b5bf6b7e64
Tyrone Davis III <td3@mit.edu>
2021-03-20 18:27:40 -0400

    refactored app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,35 +5,9 @@ import {
     Route
 } from "react-router-dom";
 import Header from "./components/layout/Header";
-import AutographDraw from "./components/AutographDraw";
-import {Flex, Box, Heading, Text, Image} from 'rebass';
-import {BuyButton, SellButton, RedeemButton} from './components/Button'
+import Landing from "./components/Landing";
 
-const souljaBoyProfilePicture = <Image
-  src={process.env.PUBLIC_URL+"/souljaboy.jpg"}
-  sx={{
-    width: 32,
-    height: 32,
-    borderRadius: 9999,
-    marginLeft: 24,
-    marginRight: 10,
-  }}
-/>;
 
-const crankThatPicture = <Image
-  src={process.env.PUBLIC_URL+"/crankthat.jpg"}
-  sx={{
-    width: [ '100%', '50%' ],
-    borderRadius: 8,
-  }}
-/>
-
-const DataField = ({title, data, url}) => {
-    return (<>
-        {title}<br />
-        {data} {url && "linkicon"}
-    </>)
-}
 function App() {
     return (
         <Router>
@@ -44,90 +18,7 @@ function App() {
                 <Switch>
                     <Route path="/">
                         <Header/>
-                        <Flex>
-                          <Box
-                            p={3}
-                            width={1/2}
-                            color='#6F6F6F'
-                            bg='primary'>
-                            <Heading
-                              fontSize={[ 5, 6, 7 ]}
-                              color='primary'>
-                              Soulja World
-                            </Heading>
-                            <Text
-                              fontSize={[ 3, 4, 5 ]}
-                              fontWeight='bold'
-                              color='primary'>
-                              <span style={{display:"flex", alignItems:"center"}}>By {souljaBoyProfilePicture} Soulja Boy</span>
-                            </Text>
-                            <Text
-                              fontSize={[ 1, 2, 3 ]}
-                              fontWeight='bold'
-                              color='primary'>
-                              Soulja Boy’s first digital album drop. Buy $CRANK tokens, sell $CRANK tokens. When you’re ready to join the club of winners, redeem for a signed digital album. You can request Soulja Boy sign your digital album.
-                            </Text>
-                            <Flex>
-                              <Box
-                                p={3}
-                                width={1/3}
-                                color="black"
-                                bg="primary">
-                                  <DataField title="Last Traded Price" data="420.69"/>
-                              </Box>
-                              <Box
-                                p={3}
-                                width={1/3}
-                                color="black"
-                                bg="primary">
-                                  <DataField title="Available" data="77/420"/>
-                              </Box>
-                              <Box
-                                p={3}
-                                width={1/3}
-                                color="black"
-                                bg="primary">
-                                  <DataField title="ERC20 Contract" data="0x00000" url="http://" />
-                              </Box>
-                            </Flex>
-                            <Flex>
-                              <Box
-                                p={3}
-                                width={1/2}
-                                color='#6F6F6F'
-                                bg='primary'>
-                                <BuyButton>
-                                    BUY
-                                </BuyButton>
-                              </Box>
-                              <Box
-                                p={3}
-                                width={1/2}
-                                color='#6F6F6F'
-                                bg='secondary'>
-                                <SellButton>
-                                    SELL
-                                </SellButton>
-                              </Box>
-                            </Flex>
-                            <RedeemButton>
-                                REDEEM
-                            </RedeemButton>
-                          </Box>
-                          <Box
-                            p={3}
-                            width={1/2}
-                            color='#6F6F6F'
-                            bg='#F1F2F6'
-                            sx={{
-                              borderRadius: 8,
-                              display: "flex",
-                              alignItems: "center",
-                              justifyContent: "center"
-                            }}>
-                            {crankThatPicture}
-                          </Box>
-                        </Flex>
+                        <Landing/>
                     </Route>
                 </Switch>
             </div>
