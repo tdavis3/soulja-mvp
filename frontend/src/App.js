e546c092fc24d573eec690ebb5bcb8831b276ef3
Evan Mays <me@evanmays.com>
2021-03-20 17:12:59 -0400

    add buttons and data to home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import {
 } from "react-router-dom";
 import Header from "./components/layout/Header";
 import AutographDraw from "./components/AutographDraw";
-import {Flex, Box, Heading, Text, Image} from 'rebass';
+import {Flex, Box, Heading, Text, Image, Button} from 'rebass';
 
 const souljaBoyProfilePicture = <Image
   src={process.env.PUBLIC_URL+"/souljaboy.jpg"}
@@ -19,6 +19,12 @@ const souljaBoyProfilePicture = <Image
   }}
 />;
 
+const DataField = ({title, data, url}) => {
+    return (<>
+        {title}<br />
+        {data} {url && "linkicon"}
+    </>)
+}
 function App() {
     return (
         <Router>
@@ -52,6 +58,52 @@ function App() {
                               color='primary'>
                               Soulja Boy’s first digital album drop. Buy $CRANK tokens, sell $CRANK tokens. When you’re ready to join the club of winners, redeem for a signed digital album. You can request Soulja Boy sign your digital album.
                             </Text>
+                            <Flex>
+                              <Box
+                                p={3}
+                                width={1/3}
+                                color="black"
+                                bg="primary">
+                                  <DataField title="Last Traded Price" data="420.69"/>
+                              </Box>
+                              <Box
+                                p={3}
+                                width={1/3}
+                                color="black"
+                                bg="primary">
+                                  <DataField title="Available" data="77/420"/>
+                              </Box>
+                              <Box
+                                p={3}
+                                width={1/3}
+                                color="black"
+                                bg="primary">
+                                  <DataField title="ERC20 Contract" data="0x00000" url="http://" />
+                              </Box>
+                            </Flex>
+                            <Flex>
+                              <Box
+                                p={3}
+                                width={1/2}
+                                color='#6F6F6F'
+                                bg='primary'>
+                                <Button className="button-buy">
+                                    BUY
+                                </Button>
+                              </Box>
+                              <Box
+                                p={3}
+                                width={1/2}
+                                color='#6F6F6F'
+                                bg='secondary'>
+                                <Button className="button-sell">
+                                    SELL
+                                </Button>
+                              </Box>
+                            </Flex>
+                            <Button className="button-redeem">
+                                REDEEM
+                            </Button>
                           </Box>
                           <Box
                             p={3}
