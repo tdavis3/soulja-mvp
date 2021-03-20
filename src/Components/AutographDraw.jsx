376ec810171bc5834aacc1a95b7a238d0fb92fff
Katz <mykatz01@gmail.com>
2021-03-20 03:14:27 -0700

    background image prop, compression after each line drawn

--- src/Components/AutographDraw.jsx
+++ src/Components/AutographDraw.jsx
@@ -1,13 +1,30 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import CanvasDraw from "react-canvas-draw";
 
-const AutographDraw = () => {
+const AutographDraw = (props) => {
+  const { imgSrc = "" } = props;
   const canvasRef = useRef(null);
+  // Keeps track of how many lines are in image
+  const [numLines, setNumLines] = useState(0);
 
   const onChange = () => {
     if (!canvasRef.current) return;
-    console.log(parseLines());
+    const saveData = JSON.parse(canvasRef.current.getSaveData());
+    setNumLines(saveData.lines.length);
   };
+
+  useEffect(() => {
+    if (!canvasRef.current) return;
+    if (numLines < 1) return;
+    // Compress recently-added line
+    const saveData = JSON.parse(canvasRef.current.getSaveData());
+    saveData.lines[saveData.lines.length - 1] = reducePrecisionOfLine(
+      saveData.lines[saveData.lines.length - 1]
+    );
+    setNumLines(saveData.lines.length);
+    canvasRef.current.loadSaveData(JSON.stringify(saveData), true);
+    console.log(parseLines());
+  }, [numLines]);
 
   const parseLines = () => {
     const outputArr = [];
@@ -25,20 +42,27 @@ const AutographDraw = () => {
     console.log(outputArr);
   };
 
+  // Returns line but with reduced precision
+  const reducePrecisionOfLine = (line) => {
+    const newPoints = [];
+    for (let i = 0; i < line.points.length; i += 8) {
+      newPoints.push(line.points[i]);
+    }
+    // Always include last point
+    newPoints.push(line.points[line.points.length - 1]);
+    return {
+      ...line,
+      points: newPoints, // replace points with newPoints
+    };
+  };
+
   // Reduces precision of savedata
   const reducePrecision = (saveData) => {
     const newLines = [];
     const lines = saveData.lines;
     lines.forEach((line) => {
-      const newPoints = [];
-      for (let i = 0; i < line.points.length; i += 8) {
-        newPoints.push(line.points[i]);
-      }
-      newPoints.push(line.points[line.points.length - 1]);
-      newLines.push({
-        ...line,
-        points: newPoints, // replace points with newPoints
-      });
+      const newLine = reducePrecisionOfLine(line);
+      newLines.push(newLine);
     });
 
     return {
@@ -60,8 +84,8 @@ const AutographDraw = () => {
         lazyRadius={2}
         ref={canvasRef}
         onChange={onChange}
+        imgSrc={imgSrc}
       />
-      <button onClick={compress}>Compress!</button>
     </div>
   );
 };
