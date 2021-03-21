dd14fcc27c162d1bda10e8416474f9394a576850
Tyrone Davis III <td3@mit.edu>
2021-03-20 22:22:47 -0400

    added input handling for forms

--- frontend/src/components/forms/RedeemForm.js
+++ frontend/src/components/forms/RedeemForm.js
@@ -1,8 +1,9 @@
+import React, {useState} from 'react';
 import {Box, Flex, Image} from "rebass";
 import {
-  Label,
-  Input,
-  Select,
+    Label,
+    Input,
+    Select,
 } from '@rebass/forms'
 import {FormButton} from "../Button";
 import {connect} from "react-redux";
@@ -18,45 +19,55 @@ const crankThatPicture = <Image
 />;
 
 const RedeemForm = ({connectWallet, metaData}) => {
-  return (
-    <Box
-      as='form'
-      onSubmit={e => e.preventDefault()}
-      py={3}>
-      <SmallHeading marginBottom={20}>Redeem</SmallHeading>
-      <Flex mx={-2} mb={3}>
-        <Box width={1 / 3} px={2}>
 
-          {crankThatPicture}
-        </Box>
-        <Box width={2 / 3} px={2}>
-          <Box sx={{display:'flex', alignItems: 'center', justifyContent:'space-between'}}>
-            <Label htmlFor='amount'><BoldText>$CRANK</BoldText></Label>
-            <Input
-              id='amount'
-              name='amount'
-              defaultValue='1'
-              width={100}
-              type="number"
-              textAlign="right"
-            />
-          </Box>
-          <Box sx={{display:'flex', alignItems: 'center', justifyContent:'space-between', marginTop:20}}>
-            <BoldText>You'll Receive</BoldText>
-            1 NFT
-          </Box>
-        </Box>
-      </Flex>
-      <Flex mx={-2} mb={3}>
-        <Box width={1} px={2}>
+    const [amountToRedeem, setAmountToRedeem] = useState(0);
 
+    const handleChange = e => {
+        setAmountToRedeem(e.target.value);
+    };
+
+    return (
+        <Box
+            as='form'
+            onSubmit={e => e.preventDefault()}
+            py={3}>
+            <SmallHeading marginBottom={20}>Redeem</SmallHeading>
+            <Flex mx={-2} mb={3}>
+                <Box width={1 / 3} px={2}>
+
+                    {crankThatPicture}
+                </Box>
+                <Box width={2 / 3} px={2}>
+                    <Box sx={{display: 'flex', alignItems: 'center', justifyContent: 'space-between'}}>
+                        <Label htmlFor='amount'><BoldText>$CRANK</BoldText></Label>
+                        <Input
+                            id='amount'
+                            name='amount'
+                            value={amountToRedeem}
+                            width={100}
+                            type="number"
+                            textAlign="right"
+                            onChange={handleChange}
+                        />
+                    </Box>
+                    <Box sx={{display: 'flex', alignItems: 'center', justifyContent: 'space-between', marginTop: 20}}>
+                        <BoldText>You'll Receive</BoldText>
+                        {amountToRedeem > 0 ? amountToRedeem : 0} NFT
+                    </Box>
+                </Box>
+            </Flex>
+            <Flex mx={-2} mb={3}>
+                <Box width={1} px={2}>
+
+                </Box>
+            </Flex>
+            <FormButton onClick={!metaData.userAddress && (() => {
+                connectWallet(metaData.web3Modal)
+            })}>
+                {metaData.userAddress ? "REDEEM" : "CONNECT WALLET"}
+            </FormButton>
         </Box>
-      </Flex>
-      <FormButton onClick={!metaData.userAddress && (() => {connectWallet(metaData.web3Modal)})}>
-        {metaData.userAddress ? "REDEEM" : "CONNECT WALLET"}
-      </FormButton>
-    </Box>
-  )
+    )
 }
 
 const mapStateToProps = (state) => {
